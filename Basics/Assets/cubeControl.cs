7bd66b80dfd7f1ec854d85a9c15b2f4ec319c15e
Malcolm Bacon <mbacon23@uwo.ca>
2018-02-06 18:41:20 -0500

    Moving cube with rigidbody movement and acceleration

--- Basics/Assets/cubeControl.cs
+++ Basics/Assets/cubeControl.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class cubeControl : MonoBehaviour {
 
+	public float speed = 10f;
+	Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
+		rb = GetComponent<Rigidbody> ();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("right")){
-			transform.Translate(0.1f,0,0); 
-		}
-		if (Input.GetKey("left")){
-			transform.Translate(-0.1f,0,0); 
-		}
-		if (Input.GetKey("up")){
-			transform.Translate(0,0,0.1f); 
-		}
-		if (Input.GetKey("down")){
-			transform.Translate(0,0,-0.1f); 
-		}
+		//Moving with keys
+//		if (Input.GetKey("right")){
+//			transform.Translate(0.1f,0,0); 
+//		}
+//		if (Input.GetKey("left")){
+//			transform.Translate(-0.1f,0,0); 
+//		}
+//		if (Input.GetKey("up")){
+//			transform.Translate(0,0,0.1f); 
+//		}
+//		if (Input.GetKey("down")){
+//			transform.Translate(0,0,-0.1f); 
+//		}
+
+		//Moving with acceleration (based on how long you hold down the keys)
+		//Multiply by time to convert from frames to m/s
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
+		rb.MovePosition (transform.position + movement);
 	}
 }
